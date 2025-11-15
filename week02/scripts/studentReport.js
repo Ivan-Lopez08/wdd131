73127dcb9487b0e8ffbc5d70767e87951870739f
Ivan-Lopez08 <andreslomejia77@gmail.com>
2025-11-15 11:56:45 -0600

    Picture album

---
+++ week02/scripts/studentReport.js
@@ -0,0 +1,9 @@
+const DAYS = 6;
+const LIMIT = 30;
+let studentReport = [11, 42, 33, 64, 29, 37, 44]
+
+for (let i = 0; i < 7; i++){
+    if (studentReport[i] < 30){
+        console.log(studentReport[i])
+    }
+}
