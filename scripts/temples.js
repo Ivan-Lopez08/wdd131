5bed03038f09db9766bfd083120fd97f4381c928
Ivan-Lopez08 <andreslomejia77@gmail.com>
2025-11-18 09:49:07 -0600

    Picture album update

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,11 @@ document.getElementById("lastModified").innerHTML = document.lastModified;
 const year = new Date().getFullYear();
 const yearSpan = document.getElementById("currentyear");
 yearSpan.textContent = year;
+
+const hamButton = document.querySelector('#burguer');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
