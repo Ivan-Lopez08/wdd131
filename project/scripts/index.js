40a473c2e870d3414fc732dab9d67964c92cfa9a
Ivan-Lopez08 <andreslomejia77@gmail.com>
2025-12-08 01:42:16 -0600

    Project V1

---
+++ project/scripts/index.js
@@ -0,0 +1,4 @@
+document.getElementById("lastModified").innerHTML = document.lastModified;
+const year = new Date().getFullYear();
+const yearSpan = document.getElementById("currentyear");
+yearSpan.textContent = year;
