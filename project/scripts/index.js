58d3c5c551598505d16bd8af7eca67039d3454fe
Ivan-Lopez08 <andreslomejia77@gmail.com>
2025-12-08 23:41:14 -0600

    Project V2

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -2,3 +2,15 @@ document.getElementById("lastModified").innerHTML = document.lastModified;
 const year = new Date().getFullYear();
 const yearSpan = document.getElementById("currentyear");
 yearSpan.textContent = year;
+
+const btn = document.getElementById("hamburgerBtn");
+  const menu = document.getElementById("mobileMenu");
+
+  btn.addEventListener("click", () => {
+    const isOpen = btn.classList.toggle("active");
+    menu.classList.toggle("open", isOpen);
+
+    // Accesibilidad
+    btn.setAttribute("aria-expanded", String(isOpen));
+    menu.setAttribute("aria-hidden", String(!isOpen));
+  });
